fe6c63711bd2227b13e03e324ffaf0a931937bcd
rawnaq94 <33788325+rawnaq94@users.noreply.github.com>
2020-07-23 09:08:20 +0300

    Update PhoneticFinder.hpp

--- PhoneticFinder.hpp
+++ PhoneticFinder.hpp
@@ -5,6 +5,5 @@ using namespace std;
 
 namespace phonetic
 {
-   std::string find( std::string text, std::string word);
-}
-int counter(char x, char y); \\מונה אותיות דומים
+   string find(string s, string word);
+};
