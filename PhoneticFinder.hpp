d15ff042788c3797bf85e03b0dad93b79833d0e2
rawnaq94 <33788325+rawnaq94@users.noreply.github.com>
2020-07-23 08:54:45 +0300

    Create PhoneticFinder.hpp

---
+++ PhoneticFinder.hpp
@@ -0,0 +1,10 @@
+#pragma once
+#include <string>
+
+using namespace std;
+
+namespace phonetic
+{
+   std::string find( std::string text, std::string word);
+}
+int counter(char a, char b); \\מונה אותיות דומים
